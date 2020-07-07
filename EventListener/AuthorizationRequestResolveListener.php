ced7d18905534c9fc8f8f51a8f38ad2074fb41df
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-07-07 16:57:24 +0900

    認可対象をシステム管理者に限定する

--- EventListener/AuthorizationRequestResolveListener.php
+++ EventListener/AuthorizationRequestResolveListener.php
@@ -3,12 +3,19 @@
 
 namespace Plugin\Api\EventListener;
 
+use Eccube\Entity\Member;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Trikoder\Bundle\OAuth2Bundle\Event\AuthorizationRequestResolveEvent;
 use Trikoder\Bundle\OAuth2Bundle\OAuth2Events;
 
 final class AuthorizationRequestResolveListener implements EventSubscriberInterface
 {
+    /**
+     * 承認する authority_id
+     * @var int
+     */
+    const APPROVE_AUTHORITY_ID = 0; //  システム管理者
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -20,10 +27,10 @@ final class AuthorizationRequestResolveListener implements EventSubscriberInterf
     {
         $user = $event->getUser();
 
-        if (null === $user) {
-            return;
+        if ($user instanceof Member && $user->getAuthority()->getId() === self::APPROVE_AUTHORITY_ID) {
+            $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+        } else {
+            $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_DENIED);
         }
-
-        $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
     }
 }
