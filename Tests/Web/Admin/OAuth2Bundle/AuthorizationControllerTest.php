85bb2eced9d855716457d05c299065c7e63d5848
kiy0taka <kiy0taka333@gmail.com>
2020-08-03 15:34:35 +0900

    認可リクエスト画面の改善

--- Tests/Web/Admin/OAuth2Bundle/AuthorizationControllerTest.php
+++ Tests/Web/Admin/OAuth2Bundle/AuthorizationControllerTest.php
@@ -15,6 +15,7 @@ namespace Plugin\Api\Tests\Web\Admin\OAuth2Bundle;
 
 use Eccube\Common\Constant;
 use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 use Trikoder\Bundle\OAuth2Bundle\Model\Client;
 
 class AuthorizationControllerTest extends AbstractAdminWebTestCase
@@ -74,16 +75,70 @@ class AuthorizationControllerTest extends AbstractAdminWebTestCase
                 'response_type' => 'code',
                 'scope' => 'read',
                 'state' => 'xxx',
+                'approve' => '',
                 Constant::TOKEN_NAME => 'dummy',
             ],
         ];
 
-        $crawler = $this->client->request(
+        $this->client->request(
             'POST', $authorize_url,
             $parameters
         );
 
-        $this->assertTrue($this->client->getResponse()->isRedirection());
+        /** @var Response $response */
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->isRedirection());
+
+        $callbackParams = $this->parseCallbackParams($response);
+
+        self::assertFalse(isset($callbackParams['error']));
+        self::assertTrue(isset($callbackParams['code']));
+    }
+
+    public function testRoutingAdminOauth2Authorize_権限移譲を許可しない()
+    {
+        /** @var Client $Client */
+        $Client = $this->entityManager->getRepository(Client::class)->findOneBy([]);
+        $authorize_url = $this->generateUrl(
+            'oauth2_authorize',
+            [
+                'client_id' => $Client->getIdentifier(),
+                'redirect_uri' => (string) current($Client->getRedirectUris()),
+                'response_type' => 'code',
+                'scope' => 'read',
+                'state' => 'xxx',
+            ]
+        );
+
+        $this->client->request('GET', $authorize_url);
+
+        $parameters = [
+            'oauth_authorization' => [
+                'client_id' => $Client->getIdentifier(),
+                'client_secret' => $Client->getSecret(),
+                'redirect_uri' => current($Client->getRedirectUris()),
+                'response_type' => 'code',
+                'scope' => 'read',
+                'state' => 'xxx',
+                'deny' => '',
+                Constant::TOKEN_NAME => 'dummy',
+            ],
+        ];
+
+        $this->client->request(
+            'POST', $authorize_url,
+            $parameters
+        );
+
+        /** @var Response $response */
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->isRedirection());
+
+        $redirectUrl = $response->headers->get('Location');
+        self::assertStringStartsWith((string) $Client->getRedirectUris()[0], $redirectUrl);
+
+        $callbackParams = $this->parseCallbackParams($response);
+        self::assertEquals('access_denied', $callbackParams['error']);
     }
 
     public function testRoutingAdminOauth2Authorize_権限移譲を許可_パラメータが足りない場合()
@@ -106,5 +161,14 @@ class AuthorizationControllerTest extends AbstractAdminWebTestCase
         );
 
         $this->assertFalse($this->client->getResponse()->isRedirection());
+    }
+
+    private function parseCallbackParams(Response $response)
+    {
+        $url = parse_url($response->headers->get('Location'));
+        $redirectParams = [];
+        parse_str($url['query'], $redirectParams);
+
+        return $redirectParams;
     }
 }
