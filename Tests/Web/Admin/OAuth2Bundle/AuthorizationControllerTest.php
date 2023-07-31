563ad6ccfa6f34573bb2338e0014131905346939
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2023-07-31 13:39:27 +0900

    Create OAuth2 client dynamically

--- Tests/Web/Admin/OAuth2Bundle/AuthorizationControllerTest.php
+++ Tests/Web/Admin/OAuth2Bundle/AuthorizationControllerTest.php
@@ -15,20 +15,29 @@ namespace Plugin\Api42\Tests\Web\Admin\OAuth2Bundle;
 
 use Eccube\Common\Constant;
 use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
+use League\Bundle\OAuth2ServerBundle\Manager\ClientManagerInterface;
+use League\Bundle\OAuth2ServerBundle\Model\Grant;
+use League\Bundle\OAuth2ServerBundle\Model\RedirectUri;
+use League\Bundle\OAuth2ServerBundle\Model\Scope;
+use League\Bundle\OAuth2ServerBundle\OAuth2Grants;
 use Symfony\Component\HttpFoundation\Response;
 use League\Bundle\OAuth2ServerBundle\Model\Client;
 
 class AuthorizationControllerTest extends AbstractAdminWebTestCase
 {
+    /** @var Client */
+    private $OAuth2Client;
+
     public function setUp(): void
     {
         parent::setUp();
+        $this->OAuth2Client = $this->createOAuth2Client();
     }
 
     public function testRoutingAdminOauth2Authorize_ログインしている場合は権限移譲確認画面を表示()
     {
         /** @var Client $Client */
-        $Client = $this->entityManager->getRepository(Client::class)->findOneBy([]);
+        $Client = $this->OAuth2Client;
 
         $this->client->request('GET',
                                $this->generateUrl(
@@ -53,7 +62,7 @@ class AuthorizationControllerTest extends AbstractAdminWebTestCase
     public function testRoutingAdminOauth2Authorize_権限移譲を許可()
     {
         /** @var Client $Client */
-        $Client = $this->entityManager->getRepository(Client::class)->findOneBy([]);
+        $Client = $this->OAuth2Client;
         $authorize_url = $this->generateUrl(
             'oauth2_authorize',
             [
@@ -98,7 +107,7 @@ class AuthorizationControllerTest extends AbstractAdminWebTestCase
     public function testRoutingAdminOauth2Authorize_権限移譲を許可しない()
     {
         /** @var Client $Client */
-        $Client = $this->entityManager->getRepository(Client::class)->findOneBy([]);
+        $Client = $this->OAuth2Client;
         $authorize_url = $this->generateUrl(
             'oauth2_authorize',
             [
@@ -170,5 +179,24 @@ class AuthorizationControllerTest extends AbstractAdminWebTestCase
         parse_str($url['query'], $redirectParams);
 
         return $redirectParams;
+    }
+
+    private function createOAuth2Client(): Client
+    {
+        $client_id = hash('md5', random_bytes(16));
+        $client_secret = hash('sha256', random_bytes(32));
+        $Client = new Client('', $client_id, $client_secret);
+        $Client
+            ->setScopes(new Scope('read'))
+            ->setRedirectUris(new RedirectUri('http://127.0.0.1:8000/'))
+            ->setGrants(
+                new Grant(OAuth2Grants::AUTHORIZATION_CODE),
+                new Grant(OAuth2Grants::REFRESH_TOKEN)
+            )
+            ->setActive(true);
+        $clientManager = self::$container->get(ClientManagerInterface::class);
+        $clientManager->save($Client);
+
+        return $Client;
     }
 }
