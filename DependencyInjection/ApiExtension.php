29411a655f53885f7d7c50410b9e26a9fdd1b660
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-08-10 15:24:10 +0900

    Customer/Member両方に対応

--- DependencyInjection/ApiExtension.php
+++ DependencyInjection/ApiExtension.php
@@ -37,8 +37,7 @@ class ApiExtension extends Extension implements PrependExtensionInterface
                             'security' => true,
                             'stateless' => true,
                             'oauth2' => true,
-                            // FIXME いったんcustomerで認証できるようにする
-                            'provider' => 'customer_provider'
+                            'provider' => 'user_provider'
                         ];
                         unset($security["firewalls"]["admin"]["form_login"]["csrf_token_generator"]);
                         unset($security["firewalls"]["admin"]["anonymous"]);
