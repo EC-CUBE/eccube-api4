c9dabf2a96c772ca888c535936b6afc04e02a8b7
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-09-14 16:48:45 +0900

    ID型に対応

---
+++ Form/Type/IdType.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\Api42\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class IdType extends TextType
+{
+}
