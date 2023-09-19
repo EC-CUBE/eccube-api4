0969e189a8c047e5f0b28ada17c500d858696444
Piers Gillingham <gillingham@sai-net.jp>
2023-09-19 22:03:52 +0900

    ログアウト機能の作成。

--- Event.php
+++ Event.php
@@ -14,6 +14,7 @@
 namespace Plugin\Api42;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -29,19 +30,27 @@ class Event implements EventSubscriberInterface
     {
         return [
             KernelEvents::RESPONSE => 'onKernelResponse',
+            KernelEvents::EXCEPTION => 'onKernelResponse',
         ];
     }
 
     /**
      * OPTIONメソッドの場合は、処理を中断する.
      *
+     * @param ExceptionEvent|ResponseEvent $event
+     *
      * @return void
      */
-    public function onKernelResponse(ResponseEvent $event)
+    public function onKernelResponse(ExceptionEvent|ResponseEvent $event): void
     {
         $request = $event->getRequest();
-        if ($request->getMethod() === 'OPTIONS' || $request->getMethod() === 'POST' && ($request->attributes->get('_route') === 'oauth2_token' || $request->attributes->get('_route') === 'oauth2_authorize') || $request->attributes->get('_route') === 'api') {
+        if ($request->getMethod() === 'OPTIONS' || $request->getMethod() === 'POST' && ($request->attributes->get('_route') === 'oauth2_token' || $request->attributes->get('_route') === 'api_logout' || $request->attributes->get('_route') === 'oauth2_authorize') || $request->attributes->get('_route') === 'api') {
+
             $response = $event->getResponse();
+            if ($response === null) {
+                return;
+            }
+
             $response->headers->add([
                 'Access-Control-Allow-Origin' => '*',
                 'Content-Type' => 'application/json',
