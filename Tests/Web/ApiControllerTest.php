6dfe85eece64cc76f4157c8c844f53323c594adf
Piers Gillingham <gillingham@sai-net.jp>
2023-09-21 14:13:03 +0900

    ログアウトに関するユニットテスト。

--- Tests/Web/ApiControllerTest.php
+++ Tests/Web/ApiControllerTest.php
@@ -19,7 +19,6 @@ use League\Bundle\OAuth2ServerBundle\Entity\RefreshToken;
 use League\Bundle\OAuth2ServerBundle\Entity\Scope;
 use League\Bundle\OAuth2ServerBundle\Manager\Doctrine\ClientManager;
 use League\Bundle\OAuth2ServerBundle\Model\Client;
-use League\Bundle\OAuth2ServerBundle\Repository\RefreshTokenRepository;
 use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\CryptKey;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
@@ -42,7 +41,7 @@ class ApiControllerTest extends AbstractWebTestCase
     /** @var AuthorizationServer */
     private $authorizationServer;
 
-    private RefreshTokenRepository $refreshTokenRepository;
+    private $refreshTokenRepository;
 
     public function setUp(): void
     {
@@ -59,10 +58,12 @@ class ApiControllerTest extends AbstractWebTestCase
      */
     public function testPermission($scopes, $query, $expectedErrorMessage = null)
     {
+        $this->deleteAllRows(['dtb_customer']);
+
         $headers = [];
         if ($scopes) {
             $token = $this->newAccessToken($scopes);
-            $headers = ['HTTP_AUTHORIZATION' => 'Bearer '.$token];
+            $headers = ['HTTP_AUTHORIZATION' => 'Bearer ' . $token];
         }
         $this->client->request('POST', $this->generateUrl('api'), [], [], $headers, json_encode(['query' => $query]));
 
@@ -95,72 +96,83 @@ class ApiControllerTest extends AbstractWebTestCase
     /**
      * @dataProvider logAndInvalidate0Auth2SessionProvider
      */
-    public function testLogoutAndInvalidateOAuth2Session(string $identifier, array $post_login_data, array $post_logout_data)
+    public function testLogoutAndInvalidateOAuth2Session(string $identifier, array $client, array $post_login_data, array $post_logout_data)
     {
+        $this->deleteAllRows(['oauth2_refresh_token', 'oauth2_access_token']);
 
-        $this->deleteAllRows(['oauth2_access_token', 'oauth2_refresh_token']);
+//        $token = $this->newAccessToken(['read:Cart', 'write:Cart', 'read:CartItem', 'write:CartItem', 'read:Category', 'read:ClassCategory', 'read:ClassName', 'read:Country', 'read:Customer', 'write:Customer', 'read:CustomerAddress', 'read:CustomerFavoriteProduct', 'read:CustomerOrderStatus', 'read:CustomerStatus', 'read:Delivery', 'read:News', 'read:Order', 'read:OrderItem', 'read:OrderItemType', 'read:OrderPdf', 'read:OrderStatus', 'read:Pref', 'read:Product', 'read:ProductCategory', 'read:ProductClass', 'read:ProductImage', 'read:ProductListMax', 'read:ProductListOrderBy', 'read:ProductStatus', 'read:ProductStock', 'read:ProductTag', 'read:Shipping']);
+
+        $customer = $this->createCustomer();
 
         $token = $this->newAccessToken(
-            scopes: [],
-            name: $identifier,
+            scopes: ['read:Cart', 'write:Cart', 'read:CartItem', 'write:CartItem', 'read:Category', 'read:ClassCategory', 'read:ClassName', 'read:Country', 'read:Customer', 'write:Customer', 'read:CustomerAddress', 'read:CustomerFavoriteProduct', 'read:CustomerOrderStatus', 'read:CustomerStatus', 'read:Delivery', 'read:News', 'read:Order', 'read:OrderItem', 'read:OrderItemType', 'read:OrderPdf', 'read:OrderStatus', 'read:Pref', 'read:Product', 'read:ProductCategory', 'read:ProductClass', 'read:ProductImage', 'read:ProductListMax', 'read:ProductListOrderBy', 'read:ProductStatus', 'read:ProductStock', 'read:ProductTag', 'read:Shipping'],
+            name: $customer->getEmail(),
             identifier: $post_login_data['oauth2_access_token']['identifier'],
             secret: $post_login_data['oauth2_access_token']['identifier'],
             expiryDateTime: $post_login_data['oauth2_access_token']['expiry'],
-            requireRefreshToken: true
+            requireRefreshToken: true,
+            type: 'password',
         );
 
-
-        $tokenList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\AccessToken::class)->findBy(['userIdentifier' => $identifier]);
+        // アクセストークンが発行されていることを確認する
+        $tokenList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\AccessToken::class)->findBy(['userIdentifier' => $customer->getEmail()]);
         self::assertCount(1, $tokenList);
         self::assertEquals($post_login_data['oauth2_access_token']['identifier'], trim($tokenList[0]->getIdentifier()));
-        self::assertEquals($post_login_data['oauth2_access_token']['expiry'], trim($tokenList[0]->getExpiry()));
-        self::assertEquals($post_login_data['oauth2_access_token']['revoked'], trim($tokenList[0]->isRevoked()));
+        self::assertEquals($post_login_data['oauth2_access_token']['expiry'], $tokenList[0]->getExpiry());
+        self::assertEquals($post_login_data['oauth2_access_token']['revoked'], $tokenList[0]->isRevoked());
 
-//        $tokenList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\RefreshToken::class)->findAll();
-
+        // リフレッシュトークンが発行されていることを確認する
         $refreshList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\RefreshToken::class)->findBy(['accessToken' => $post_login_data['oauth2_access_token']['identifier']]);
         self::assertCount(1, $refreshList);
         self::assertEquals($post_login_data['oauth2_refresh_token']['access_token'], trim($refreshList[0]->getAccessToken()));
-        self::assertEquals($post_login_data['oauth2_refresh_token']['expiry'], trim($refreshList[0]->getExpiry()));
-        self::assertEquals($post_login_data['oauth2_refresh_token']['revoked'], trim($refreshList[0]->isRevoked()));
+        self::assertEquals($post_login_data['oauth2_refresh_token']['expiry'], $refreshList[0]->getExpiry());
+        self::assertEquals($post_login_data['oauth2_refresh_token']['revoked'], $refreshList[0]->isRevoked());
 
-        $headers = ['HTTP_AUTHORIZATION' => 'Bearer '.$token];
+        $headers = ['HTTP_AUTHORIZATION' => 'Bearer ' . $token];
         $this->client->request('POST', $this->generateUrl('api_logout'), [], [], $headers);
 
+        if ($post_logout_data['oauth2_access_token']['deleted'] === true) {
+            // トークンが削除されている場合は、トークンが無効であることを確認する
+            self::assertEquals(401, $this->client->getResponse()->getStatusCode());
+            return;
+        }
+
+        // トークンが削除されていない場合は、トークンが無効化されていることを確認する
         self::assertEquals(200, $this->client->getResponse()->getStatusCode());
         self::assertEquals('application/json', $this->client->getResponse()->headers->get('Content-Type'));
-        self::assertEquals('{"success":true}', $this->client->getResponse()->getContent());
+        self::assertEquals('{"message":"success"}', $this->client->getResponse()->getContent());
 
-        $tokenList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\AccessToken::class)->findBy(['userIdentifier' => $identifier]);
-        if ($post_logout_data['oauth2_access_token']['deleted'] === true) {
-            self::assertCount(0, $tokenList);
-        } else {
-            self::assertCount(1, $tokenList);
-            self::assertEquals($post_logout_data['oauth2_access_token']['identifier'], trim($tokenList[0]->getIdentifier()));
-            self::assertEquals($post_logout_data['oauth2_access_token']['expiry'], trim($tokenList[0]->getExpiry()));
-            self::assertEquals($post_logout_data['oauth2_access_token']['revoked'], trim($tokenList[0]->isRevoked()));
-        }
+        $tokenList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\AccessToken::class)->findBy(['userIdentifier' => $customer->getEmail()]);
+        self::assertCount(1, $tokenList);
+        self::assertEquals($post_logout_data['oauth2_access_token']['identifier'], trim($tokenList[0]->getIdentifier()));
+        self::assertEquals($post_logout_data['oauth2_access_token']['expiry'], $tokenList[0]->getExpiry());
+        self::assertEquals($post_logout_data['oauth2_access_token']['revoked'], $tokenList[0]->isRevoked());
 
         $refreshList = $this->entityManager->getRepository(\League\Bundle\OAuth2ServerBundle\Model\RefreshToken::class)->findBy(['accessToken' => $post_login_data['oauth2_access_token']['identifier']]);
-        if ($post_logout_data['oauth2_refresh_token']['deleted'] === true) {
-            self::assertCount(0, $refreshList);
-        } else {
-            self::assertCount(1, $refreshList);
-            self::assertEquals($post_logout_data['oauth2_refresh_token']['access_token'], trim($refreshList[0]->getAccessToken()));
-            self::assertEquals($post_logout_data['oauth2_refresh_token']['expiry'], trim($refreshList[0]->getExpiry()));
-            self::assertEquals($post_logout_data['oauth2_refresh_token']['revoked'], trim($refreshList[0]->isRevoked()));
-        }
+        self::assertCount(1, $refreshList);
+        self::assertEquals($post_logout_data['oauth2_refresh_token']['access_token'], trim($refreshList[0]->getAccessToken()));
+        self::assertEquals($post_logout_data['oauth2_refresh_token']['expiry'], $refreshList[0]->getExpiry());
+        self::assertEquals($post_logout_data['oauth2_refresh_token']['revoked'], $refreshList[0]->isRevoked());
     }
 
     public function logAndInvalidate0Auth2SessionProvider(): array
     {
         $clientInfo = [
             'test_active_user@example.com' => [
+                'client' => [
+                    'identifier' => hash('md5', random_bytes(16)),
+                    'secret' => hash('sha512', random_bytes(32)),
+                    'redirect_uri' => 'http://localhost:8080' . $this->generateUrl('homepage'),
+                ],
                 'identifier' => hash('md5', random_bytes(16)),
-                'secret' => hash('sha512', random_bytes(32)),
                 'expiry' => new \DateTimeImmutable('+1 days', new \DateTimeZone('Asia/Tokyo')),
             ],
             'inactive_user@example.com' => [
+                'client' => [
+                    'identifier' => hash('md5', random_bytes(16)),
+                    'secret' => hash('sha512', random_bytes(32)),
+                    'redirect_uri' => 'http://localhost:8080' . $this->generateUrl('homepage'),
+                ],
                 'identifier' => hash('md5', random_bytes(16)),
                 'secret' => hash('sha512', random_bytes(32)),
                 'expiry' => new \DateTimeImmutable('-1 days', new \DateTimeZone('Asia/Tokyo')),
@@ -170,6 +182,7 @@ class ApiControllerTest extends AbstractWebTestCase
         return [
             [
                 'identifier' => 'test_active_user@example.com',
+                'client' => $clientInfo['test_active_user@example.com']['client'],
                 'post_login_data' => [
                     'oauth2_access_token' => [
                         'user_identifier' => 'test_active_user@example.com',
@@ -202,6 +215,7 @@ class ApiControllerTest extends AbstractWebTestCase
                 ],
             ], [
                 'identifier' => 'inactive_user@example.com',
+                'client' => $clientInfo['inactive_user@example.com']['client'],
                 'post_login_data' => [
                     'oauth2_access_token' => [
                         'user_identifier' => 'inactive_user@example.com',
@@ -241,6 +255,7 @@ class ApiControllerTest extends AbstractWebTestCase
      * @param string $secret
      * @param \DateTimeImmutable|null $expiryDateTime
      * @param bool $requireRefreshToken
+     * @param string $type
      *
      * @return string
      *
@@ -255,7 +270,7 @@ class ApiControllerTest extends AbstractWebTestCase
             $secret = hash('sha512', random_bytes(32));
         }
 
-        $client = new Client($name, $identifier, $secret);
+        $client = new Client('', $identifier, $secret);
         $client->setScopes(...array_map(function ($s) {
             return new \League\Bundle\OAuth2ServerBundle\Model\Scope($s);
         }, $scopes));
@@ -267,7 +282,7 @@ class ApiControllerTest extends AbstractWebTestCase
         $accessTokenEntity->setClient($clientEntity);
         $accessTokenEntity->setExpiryDateTime($expiryDateTime ?? new \DateTimeImmutable('+1 days', new \DateTimeZone('Asia/Tokyo')));
         $accessTokenEntity->setUserIdentifier($name !== '' ? $name : 'admin');
-        $accessTokenEntity->setPrivateKey(new CryptKey(self::$container->getParameter('kernel.project_dir').'/app/PluginData/Api42/oauth/private.key'));
+        $accessTokenEntity->setPrivateKey(new CryptKey(self::$container->getParameter('kernel.project_dir') . '/app/PluginData/Api42/oauth/private.key'));
 
         array_walk($scopes, function ($s) use ($accessTokenEntity) {
             $scope = new Scope();
@@ -280,7 +295,6 @@ class ApiControllerTest extends AbstractWebTestCase
         if ($requireRefreshToken === true) {
             $this->newRefreshToken(accessTokenEntity: $accessTokenEntity, expiry: $expiryDateTime);
         }
-
 
         $this->entityManager->flush();
 
@@ -299,6 +313,5 @@ class ApiControllerTest extends AbstractWebTestCase
         $refreshTokenEntity->setAccessToken($accessTokenEntity);
         $refreshTokenEntity->setExpiryDateTime($expiry);
         $this->refreshTokenRepository->persistNewRefreshToken($refreshTokenEntity);
-
     }
 }
